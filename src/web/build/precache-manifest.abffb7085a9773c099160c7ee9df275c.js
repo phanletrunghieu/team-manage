e23624a96df4882ae6dccd85d33d54f2f5cacb5e
Phan Hieu <hieutrunglephan@gmail.com>
2018-11-17 04:42:04 +0700

    add react

---
+++ src/web/build/precache-manifest.abffb7085a9773c099160c7ee9df275c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be8f3cd043658e8a5168",
+    "url": "/static/js/main.be8f3cd0.chunk.js"
+  },
+  {
+    "revision": "c4cc7419905f603656d1",
+    "url": "/static/js/1.c4cc7419.chunk.js"
+  },
+  {
+    "revision": "be8f3cd043658e8a5168",
+    "url": "/static/css/main.e07bcba9.chunk.css"
+  },
+  {
+    "revision": "7a4076f7734fda9675de4e20e3648205",
+    "url": "/index.html"
+  }
+];
